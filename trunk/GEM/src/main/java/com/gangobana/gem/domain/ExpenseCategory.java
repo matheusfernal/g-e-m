8f3de27d5551929926b9c95d77082ab3f3714178
matheusfernal <matheusfernal@6683f872-2a48-0410-905a-5b7c5a519ae3>
2008-08-15 01:42:39 +0000

    Interacting with a cell

--- trunk/GEM/src/main/java/com/gangobana/gem/domain/ExpenseCategory.java
+++ trunk/GEM/src/main/java/com/gangobana/gem/domain/ExpenseCategory.java
@@ -2,33 +2,38 @@ package com.gangobana.gem.domain;
 
 public enum ExpenseCategory {
 	
-	ALIMENTACAO("Alimentação"),
-	ALUGUEL("Aluguel"),
-	CONDOMINIO("Condomínio"),
-	SAUDE("Saúde"),
-	TRANSPORTE("Transporte"),
-	INTERNET("Internet"),
-	TELEFONE("Telefone"),
-	AGUA("Água"),
-	LUZ("Luz"),
-	VESTUARIO("Vestuário"),
-	PRESENTES("Presentes"),
-	LAZER("Lazer"),
-	SUPERMERCADO("Supermercado"),
-	CARTAO_DE_CREDITO("Cartão de Crédito"),
-	GASTOS_GANGOBANA("Gastos Gangobana"),
-	IMPOSTOS_PESSOAIS("Impostos Pessoais"),
-	OUTRAS_DESPESAS("Outras Despesas");
+	ALIMENTACAO("Alimentação", 2),
+	ALUGUEL("Aluguel", 3),
+	CONDOMINIO("Condomínio", 4),
+	SAUDE("Saúde", 5),
+	TRANSPORTE("Transporte", 6),
+	INTERNET("Internet", 7),
+	TELEFONE("Telefone", 8),
+	AGUA("Água", 9),
+	LUZ("Luz", 10),
+	VESTUARIO("Vestuário", 11),
+	PRESENTES("Presentes", 12),
+	LAZER("Lazer", 13),
+	SUPERMERCADO("Supermercado", 14),
+	CARTAO_DE_CREDITO("Cartão de Crédito", 15),
+	GASTOS_GANGOBANA("Gastos Gangobana", 16),
+	IMPOSTOS_PESSOAIS("Impostos Pessoais", 17),
+	OUTRAS_DESPESAS("Outras Despesas", 18);
 	
 	private final String name;
+	private final int column;
 	
-	private ExpenseCategory(String name) {
-		
+	private ExpenseCategory(String name, int column) {
 		this.name = name;
+		this.column = column;
 	}
 	
 	@Override
 	public String toString() {
 		return name;
+	}
+	
+	public int getColumn() {
+		return column;
 	}
 }
